a58148459e1dc043232273497edecc3d29ab1165
Thomas Weustenfeld <tw@econify.com>
2021-11-29 13:09:34 +0100

    [TW] Example router

--- examples/simple-router/index.ts
+++ examples/simple-router/index.ts
@@ -1,23 +1,29 @@
 /* eslint-disable import/no-relative-parent-imports */
+import GraphQLWebsocketRouter from "../../src";
+import { App as uWebSocketApp, DEDICATED_COMPRESSOR_3KB } from "uWebSockets.js";
 import { readFileSync } from "fs";
 import { resolve } from "path";
-import GraphQLWebsocketRouter from "../../src";
-import { createSocketServer } from "../../src/usocket";
 
-const clientQueries = readFileSync(
-  resolve(__dirname, "./example-queries.graphql"),
-  "utf-8"
-);
+// const api = new GraphQLWebsocketRouter(
+//   "https://rickandmortyapi.com/graphql",
+//   readFileSync(resolve(__dirname, "./example-queries.graphql"), "utf-8")
+// );
 
 const api = new GraphQLWebsocketRouter(
-  "https://rickandmortyapi.com/graphql",
-  clientQueries
+  "ws://localhost:4000/graphql",
+  readFileSync(resolve(__dirname, "../backend/queries.graphql"), "utf-8")
 );
 
-const responder = api.mountAll().asMessageResponders();
-
-createSocketServer(responder).listen(9001, (listenSocket) => {
-  if (listenSocket) {
-    console.log("Listening to port 9001");
-  }
-});
+uWebSocketApp()
+  .ws("/*", {
+    idleTimeout: 8 * 4,
+    maxBackpressure: 1024,
+    maxPayloadLength: 512,
+    compression: DEDICATED_COMPRESSOR_3KB,
+    ...api.uWebSocketBehavior(),
+  })
+  .listen(9001, (listenSocket) => {
+    if (listenSocket) {
+      console.log("Listening to port 9001");
+    }
+  });
